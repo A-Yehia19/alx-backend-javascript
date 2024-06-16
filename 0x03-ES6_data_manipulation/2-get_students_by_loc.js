a4558f31eef91f94f43574525beb64c2ceb825bc
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-06-16 15:04:33 +0300

    fixed issues

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,7 @@
 export default function getStudentsByLocation(students, location) {
-  if (students instanceof Array) { return students.filter((student) => student.location == location); }
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === location);
+  }
 
   return [];
 }
